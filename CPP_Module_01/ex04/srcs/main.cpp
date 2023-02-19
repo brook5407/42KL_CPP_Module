8dbc1a9c3c3a1f0b685289618adf6b9ad60f769e
Brook <chunyong96@gmail.com>
2023-02-19 12:01:54 +0800

    handle unexpected input

--- CPP_Module_01/ex04/srcs/main.cpp
+++ CPP_Module_01/ex04/srcs/main.cpp
@@ -16,7 +16,7 @@
 int	main(int argc, char **argv){
 	// Check for the correct number of arguments
 	if (argc != 4){
-		std::cerr << "\033[1;31mUsage: " << argv[0] << " <filename> <s1> <s2>\033[0m" << std::endl;
+		std::cerr << "\033[1;31mUsage: " << argv[0] << " <filename> <string for replace> <string to replace>\033[0m" << std::endl;
 		return (EXIT_FAILURE);
 	}
 	// Get the filename, s1, and s2 from the command line arguments
@@ -35,19 +35,24 @@ int	main(int argc, char **argv){
 		std::cerr << "\033[1;31mError: Unable to open file " << filename << std::endl;
 		return (EXIT_FAILURE);
 	}
-	// Copy the contents of the input file to the output file, replacing s1 with s2
-	std::string line;
-	while (getline(ifs, line)) {
-		size_t pos = 0;
-		while (1) {
-			pos = line.find(s1, pos);
-			if (pos == std::string::npos) {
-				ofs << line << std::endl;
-				break;
+	// Copy contents of input file to output file since nothing could replace
+	if (s1.empty())
+		ofs << ifs.rdbuf();
+	else {
+		// Copy the contents of the input file to the output file, replacing s1 with s2
+		std::string line;
+		while (getline(ifs, line)) {
+			size_t pos = 0;
+			while (1) {
+				pos = line.find(s1, pos);
+				if (pos == std::string::npos) {
+					ofs << line << std::endl;
+					break;
+				}
+				line.erase(line.begin() + pos, line.begin() + pos + s1.length());
+				line.insert(pos, s2);
+				pos += s2.length();
 			}
-			line.erase(line.begin() + pos, line.begin() + pos + s1.length());
-			line.insert(pos, s2);
-			pos += s2.length();
 		}
 	}
 	// Close the input and output files
